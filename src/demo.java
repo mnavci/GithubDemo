c3ba7543debfb27c985aa4842f93faf947025c4e
Nuri Avci <nuriavci@Mehmets-Air.fios-router.home>
2021-08-02 13:23:08 -0400

    We have explained or/and in if condition

--- src/demo.java
+++ src/demo.java
@@ -1,2 +1,13 @@
 public class demo {
+    public static void main(String[] args) {
+
+        int a=5;
+        int b=3;
+        if(a==3 || a<3){
+            System.out.println();
+        }
+        if (a<3 && a<5){
+
+        }
+    }
 }
